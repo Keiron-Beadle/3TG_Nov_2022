930ef5c6f8209b074540437dff775f549c3392bb
Keiron-Beadle <56656042+Keiron-Beadle@users.noreply.github.com>
2022-11-26 10:47:00 +0000

    Added quit listener (ESC)

--- Game/Player_Controller.gd
+++ Game/Player_Controller.gd
@@ -8,11 +8,11 @@ var mouse_sens = 0.002;
 
 var velocity = Vector3()
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
 	
+
 func get_input():
 	var wish_dir = Vector3()
 	if Input.is_action_pressed("forward"):
@@ -23,17 +23,23 @@ func get_input():
 		wish_dir += -global_transform.basis.x
 	if Input.is_action_pressed("strafe_right"):
 		wish_dir += global_transform.basis.x
-	wish_dir = wish_dir.normalize()
+	wish_dir = wish_dir.normalized()
 	return wish_dir
-	
+
 func _unhandled_input(event):
 	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		#Rotate Bodies Y
 		rotate_y(-event.relative.x * mouse_sens) 
 		#Rotate Pivot X (Don't want model to rotate Y)
 		$Pivot.rotate_x(-event.relative.y * mouse_sens)
+		#Clamp X rotation between these values so we can't invert the view. 
 		$Pivot.rotation.x = clamp($Pivot.rotation.x, -1.2, 1.2)
-	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+		var viewport = get_viewport()
+		#viewport.warp_mouse(viewport.get_rect().size)
+
+func _physics_process(delta):
+	velocity.y += gravity * delta
+	var wish_vel = get_input() * max_speed
+	velocity.x = wish_vel.x
+	velocity.z = wish_vel.z
+	velocity = move_and_slide(velocity, Vector3.UP, true)
