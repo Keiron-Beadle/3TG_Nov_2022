c43a8b3753604a3623793b60383478e20657a3ff
Keiron-Beadle <56656042+Keiron-Beadle@users.noreply.github.com>
2022-11-26 13:03:43 +0000

    Holding space will preserve forward momentum

--- Game/Player_Controller.gd
+++ Game/Player_Controller.gd
@@ -9,7 +9,7 @@ var velocity = Vector3()
 var jump_add = 0.34
 var speed = 3
 var max_add = 1
-var max_dec = 3
+var max_dec = 10
 var max_speed = 100
 
 func _ready():
@@ -25,18 +25,15 @@ func get_input():
 		wish_dir += -global_transform.basis.x
 	if Input.is_action_pressed("strafe_right"):
 		wish_dir += global_transform.basis.x
-	wish_dir = wish_dir.normalized()
 	if Input.is_action_pressed("jump"):
+		var wishLenSqrd = wish_dir.length_squared()
+		if wishLenSqrd < 0.1 and wishLenSqrd > -0.1:
+			wish_dir.x = velocity.x
+			wish_dir.z = velocity.z
 		if is_on_floor():
-			velocity.y = 6
-	else:
-		if is_on_floor():
-			velocity.y = 0
-	if not is_on_floor():
-		if wish_dir.length_squared() == 0:
-			wish_dir = velocity.normalized()
+			velocity.y = 20
 
-	return wish_dir
+	return (wish_dir.normalized())
 
 func _unhandled_input(event):
 	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
@@ -53,18 +50,22 @@ func _physics_process(delta):
 	velocity.y += gravity * delta
 	var wish_dir = get_input()
 	var wish_dirV2 = Vector2(wish_dir.x, wish_dir.z)
-	var vel_dirV2 = Vector2(velocity.x, velocity.z).normalized()
-	var angleBetweenVelAndWish = wish_dirV2.angle_to(vel_dirV2)
+	var vel_dirV2 = (Vector2(velocity.x, velocity.z).normalized())
+	var angleBetweenVelAndWish = abs(wish_dirV2.angle_to(vel_dirV2))
 	
 	var speedIncrease = smoothstep(0.01, 0.698, angleBetweenVelAndWish)
-	var speedDecrease = smoothstep(0.699, 1.0472, angleBetweenVelAndWish)
+	var speedDecrease = -smoothstep(0.699, 0.872, angleBetweenVelAndWish)
 	
-	speed = clamp(speedIncrease * max_add + speedDecrease * max_dec,3,max_speed)
-	
+	speed += clamp(speedIncrease * max_add + speedDecrease * max_dec,-max_dec,max_add)
+	speed = clamp(speed,3, max_speed)
 	var wish_vel = wish_dir * speed
-
+	if wish_vel.x == 0 and wish_vel.z == 0:
+		pass
 	velocity.x = wish_vel.x
 	velocity.z = wish_vel.z
-	var label = get_node("/root/Spatial/UI/Velocity_Label")
-	label.text = "Velocity: " + str(velocity.length())
+	var velText = Vector2(velocity.x, velocity.z).length()
+	get_node("/root/Spatial/UI/Velocity_Label").text = "Velocity: " + str(velText)
+	get_node("/root/Spatial/UI/Speed_Label").text = "Speed: " + str(speed)
+	get_node("/root/Spatial/UI/SpeedInc_Label").text = "Inc: " + str(speedIncrease)
+	get_node("/root/Spatial/UI/SpeedDec_Label").text = "Dec: " + str(speedDecrease)
 	velocity = move_and_slide(velocity, Vector3.UP, true)
